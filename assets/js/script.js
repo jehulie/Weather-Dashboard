83ab216f72ba53c18489ce83a23cfe94ed8a52a0
Julie Chu <julieychu@gmail.com>
2022-01-20 17:33:16 -0600

    add weather API for current temp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
+var todayTemp = document.getElementById('current-temp');
+var todayWind = document.getElementById('current-wind');
+var todayHumid = document.getElementById('current-humid');
+var todayUV = document.getElementById('current-uv');
+var todayCity = document.getElementById('city-header');
+
 // Pulling city location from html
-var userCity = document.getElementById('userCity');
+var userCity = document.getElementById('userCity').value;
+console.log(userCity);
 var userCity_test = 'New York';
+var submitBtn = document.getElementById('custom-btn');
+
+// Function for what happens when the user clicks the button to show them weather by city input
+submitBtn.onclick = function () { submitHandler() };
+
+function submitHandler() {
+  userCity = document.getElementById('userCity').value;
+  localStorage.setItem('citySearch', userCity);
+  document.getElementsByClassName('list-group').innerHTML = localStorage.getItem('searchCity');
+};
 
 // Using Open Weather Geocode API for convering city to longitude & latitude
 var geocodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity_test + '&appid=bec1cdd13d275e4702f754866932c17f'
@@ -22,72 +39,69 @@ function geocode() {
     });
 };
 
-// geocode();
+geocode();
 
-// function getLngLat(data) {
-//   var userLat = data[0].lat;
-//   var userLng = data[0].lon;
-//   console.log("latitude:", userLat);
-//   console.log("longitude:", userLng);
-//   var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + userLat + '&lon=' + userLng + '&appid=bec1cdd13d275e4702f754866932c17f&units=imperial';
-//   getApi(oneCallUrl, data);
-// };
+function getLngLat(data) {
+  var userLat = data[0].lat;
+  var userLng = data[0].lon;
+  console.log("latitude:", userLat);
+  console.log("longitude:", userLng);
+  var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + userLat + '&lon=' + userLng + '&appid=bec1cdd13d275e4702f754866932c17f&units=imperial';
+  getApi(oneCallUrl, data);
+};
 
 // Get Weather data from Open Weather One Call API
-// function getApi(oneCallUrl) {
-//   fetch(oneCallUrl)
-//     .then(function (response) {
-//       if (response.status === 200) {
-//       }
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data)
-      // displayWeather(data);
-//       return data;
-//     })
-//     //   catch any errors
-//     .catch(function () {
-//     });
-// };
+function getApi(oneCallUrl) {
+  fetch(oneCallUrl)
+    .then(function (response) {
+      if (response.status === 200) {
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      displayWeather(data);
+      displayForecast(data);
+      return data;
+    })
+    //   catch any errors
+    .catch(function () {
+    });
+};
 
-// Function to display weather data on web page
-// function displayWeather(data) {
-//   var todayTemp = document.getElementsByClassName('current-temp');
-//   var todayWind = document.getElementsByClassName('current-wind');
-//   var todayHumid = document.getElementsByClassName('current-humid');
-//   var todayUV = document.getElementsByClassName('current-uv');
+// Function to display current weather data on web page
+function displayWeather(data) {
+  var currentTemp = data.current.temp;
+  console.log('Temp:', currentTemp);
+  var currentWind = data.current.wind_speed;
+  console.log('Wind:', currentWind);
+  var currentHumid = data.current.humidity;
+  console.log('Humidity:', currentHumid);
+  var currentUV = data.current.uvi;
+  console.log('UV:', currentUV);
+  let Temp = currentTemp.toFixed(1);
+  todayCity.textContent = userCity_test;
+  todayTemp.textContent = "Temp: " + Temp + '\u00B0F ';
+  // The code below for todayWind is not working...
+  todayWind.textContent = "Wind: " + currentWind + " MPH";
+  todayHumid.textContent = "Humidity: " + currentHumid + " %";
+  todayUV.textContent = "UV Index: " + currentUV;
+  return data;
+};
 
-//   var currentTemp = data.current.temp;
-//   var currentWind = data.current.wind_speed;
-//   var currentHumid = data.current.humidity;
-//   var currentUV = data.current.uvi;
-//   var iconCode = data.current.weather[0].icon;
-//   var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
-//   let Temp = currentTemp.toFixed(1);
-//   todayTemp.textContent = "Temp: " + Temp + '\u00B0F ';
-//   todayWind.texContent = "Wind: " + currentWind + "MPH";
-//   todayHumid.textContent = "Humidity: " + currentHumid + " %";
-//   todayUV.textContent = "UV Index: " + currentUV;
-
-// Convert dt to date
-// const dt = data.current.dt;
-// var day = new Date(dt*1000);
-// console.log(day.toDateString());
-
-// Option 2:
-// const unixTimestamp = data.current.dt
-// const date = new Date(unixTimestamp * 1000);
-// const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-// const year = date.getFullYear();
-// const month = months[date.getMonth()];
-// const dt = date.getDate();
-// const hours = date.getHours();
-// const minutes = "0" + date.getMinutes();
-// const seconds = "0" + date.getSeconds();
-// const formattedTime = `${month}-${dt}-${year}}`;
-
-// console.log(formattedTime);
-
-// }
-
+// Function to display 5-day forecast on web page
+function displayForecast (data) {
+    // Convert daily dt to legible date for 5-day forecast
+    const dailyDt = data.daily[1].dt;
+    const date = new Date(dailyDt * 1000);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const dailyTime = `${month}/${day}/${year}`;
+    console.log(dailyTime);
+    // Pull weather icon for 5-day forecast
+    var iconCode = data.daily[1].weather[0].icon;
+    var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+    console.log(iconUrl);
+    $('#wicon').attr('src', iconUrl);
+};
