53fd55feec084ff3e98ecde71a95fa0a33b7ca8e
Julie Chu <julieychu@gmail.com>
2022-01-24 01:29:00 -0600

    update search results and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,47 +3,49 @@ var todayWind = document.getElementById('current-wind');
 var todayHumid = document.getElementById('current-humid');
 var todayUV = document.getElementById('current-uv');
 var todayCity = document.getElementById('city-header');
+var todayDate = document.getElementById('current-date');
+var forecastDisplay = document.getElementById('forecast-display');
+var forecastCard = document.getElementById('forecast-card');
+var forecastDate = document.getElementById('date-input');
+var forecastIcon = document.getElementById('wicon');
 var forecastTemp = document.getElementById('daily-temp');
 var forecastWind = document.getElementById('daily-wind');
 var forecastHumid = document.getElementById('daily-humid');
-var forecastDate = document.getElementById('date-input');
-var searchResults = document.getElementsByClassName('list-group');
+var searchResults = document.getElementById('history');
+var form = document.getElementById('search-form');
+var searchHistory = [];
+var arrayCards = [];
+var arrayDataObject = [];
 
-// Pulling city location from html
-var userCity = "";
-var userCity_test = 'New York';
-var submitBtn = document.getElementById('custom-btn');
+// Pulling city location from user input in HTML
+var userCity = document.getElementById('userCity');
 
-// Function for what happens when the user clicks the button to show them weather by city input
-// ** Can't get this to return input value to var userCity **
-function getUserInput() {
-  userCity = document.getElementById('userCity').value;
-  console.log(userCity);
-
-  // ** Can't get local storage to save an array of userCity inputs, only saves first input **
-  var CityArray = localStorage.getItem("savedCity") || [];
-  CityArray.push(userCity);
-  localStorage.setItem('savedCity', CityArray);
+// Saves user city inputs into local storage and calls functions to get weather data and display results
+function getUserInput(city) {
+  searchHistory.push(city);
+  localStorage.setItem('savedCity', JSON.stringify(searchHistory));
+  geocode(city);
   loadCities()
 };
 
-// ** Can't get local storage to load prior search inputs **
+// Displays user inputs for prior city searches on web page
+//  ** How to delete duplicate from local storage and in the display
 function loadCities() {
-  searchResults.setAttribute("value", localStorage);
-  var storedValue = localStorage.getItem("savedCity");
-  for (var i = 0; i < storedValue.length; i++) {
-    var listEl = document.createElementNS('li')
-    listEl.textContent = storedValue[i]
-    searchResults.appendChild(listEl);
+  searchResults.innerHTML = "";
+  // searchResults.setAttribute("value", localStorage);
+  // var storedValue = localStorage.getItem("savedCity");
+  for (var i = 0; i < searchHistory.length; i++) {
+    var buttonEl = document.createElement('button');
+    buttonEl.setAttribute("value", searchHistory[i]);
+    buttonEl.setAttribute("class", "search-button")
+    buttonEl.textContent = searchHistory[i];
+    searchResults.appendChild(buttonEl);
   }
 };
 
-// getUserInput();
-
-// Using Open Weather Geocode API for convering city to longitude & latitude
-var geocodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userCity_test + '&appid=bec1cdd13d275e4702f754866932c17f'
-
-function geocode() {
+// Using Open Weather Geocode API for converting city to longitude & latitude
+function geocode(city) {
+  var geocodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=bec1cdd13d275e4702f754866932c17f'
   fetch(geocodeUrl)
     .then(function (response) {
       if (response.status === 200) {
@@ -60,13 +62,20 @@ function geocode() {
     });
 };
 
-geocode();
-
 function getLngLat(data) {
   var userLat = data[0].lat;
   var userLng = data[0].lon;
+  var cityInput = data[0].local_names.en
+  var currentdate = new Date();
+  var month = String(currentdate.getMonth() + 1).padStart(2, '0');
+  var day = String(currentdate.getDate()).padStart(2, '0');
+  var year = currentdate.getFullYear();
+  var currentDate = month + "/" + day + "/" + year;
   console.log("latitude:", userLat);
   console.log("longitude:", userLng);
+  console.log('Today:', currentDate);
+  console.log("city:", cityInput);
+  todayCity.textContent = cityInput + " (" + currentDate + ")"
   var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + userLat + '&lon=' + userLng + '&appid=bec1cdd13d275e4702f754866932c17f&units=imperial';
   getApi(oneCallUrl, data);
 };
@@ -82,8 +91,8 @@ function getApi(oneCallUrl) {
     .then(function (data) {
       console.log(data)
       displayWeather(data);
-      displayIcon(data);
-      displayDate(data);
+      // displayIcon(data);
+      // displayDate(data);
       displayForecast(data);
       return data;
     })
@@ -94,11 +103,6 @@ function getApi(oneCallUrl) {
 
 // Function to display current weather data on web page
 function displayWeather(data) {
-  var currentdate = new Date();
-  var month = String(currentdate.getMonth() + 1).padStart(2, '0');
-  var day = String(currentdate.getDate()).padStart(2, '0');
-  var year = currentdate.getFullYear();
-  var currentDate = month + "/" + day + "/" + year;
   var currentTemp = data.current.temp;
   console.log('Temp:', currentTemp);
   var currentWind = data.current.wind_speed;
@@ -108,11 +112,10 @@ function displayWeather(data) {
   var currentUV = data.current.uvi;
   console.log('UV:', currentUV);
   let Temp = currentTemp.toFixed(1);
-  todayCity.textContent = userCity_test + " (" + currentDate + ")";
   todayTemp.textContent = "Temp: " + Temp + '\u00B0F ';
   todayWind.textContent = "Wind: " + currentWind + " MPH";
   todayHumid.textContent = "Humidity: " + currentHumid + " %";
-  todayUV.textContent = " " + currentUV + " ";
+  todayUV.textContent = "  " + currentUV + "  ";
   // set color of UV index for favorable (0-2), moderate (3-5), severe (6+)
   // ** how to fix spacing for highlighted color **
   if (currentUV < 3) {
@@ -127,20 +130,13 @@ function displayWeather(data) {
   return data;
 };
 
-// Pull weather icon for 5-day forecast
-function displayIcon(data) {
+// Function to display 5-day forecast on web page
+function displayForecast(data) {
+  // clear elements in forecastDisplay
+  forecastCard.innerHTML = "";
+  //  loop function to get data to populate 5-day forecast
   for (let i = 1; i < 6; i++) {
-    var iconCode = data.daily[i].weather[0].icon;
-    var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
-    console.log(iconUrl);
-    // ** How do I add array of icons to each #wicon in HTML?**
-    $('#wicon').attr('src', iconUrl)
-  }
-};
-
-// Convert daily dt to legible date for 5-day forecast
-function displayDate(data) {
-  for (let i = 1; i < 6; i++) {
+    // Convert daily dt to legible date for 5-day forecast
     const dailyDt = data.daily[i].dt;
     const date = new Date(dailyDt * 1000);
     const year = date.getFullYear();
@@ -148,14 +144,11 @@ function displayDate(data) {
     const day = date.getDate();
     const dailyTime = `${month}/${day}/${year}`;
     console.log("Date:", dailyTime);
-    // ** How do I add array of dailyTime to each forecastDate in HTML?**
-    forecastDate.textContent = dailyTime;
-  }
-};
-
-// Function to display 5-day forecast on web page
-function displayForecast(data) {
-  for (let i = 1; i < 6; i++) {
+    // Get weather icon
+    var iconCode = data.daily[i].weather[0].icon;
+    var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png';
+    console.log(iconUrl);
+    // Get data for temperature, wind, and humidity
     var dailyTemp = data.daily[i].temp.day;
     console.log('Temp:', dailyTemp);
     var dailyWind = data.daily[i].wind_speed;
@@ -164,11 +157,46 @@ function displayForecast(data) {
     console.log('Humidity:', dailyHumid);
     let dTemp = dailyTemp.toFixed(1);
     console.log(dTemp);
-    // ** How do I add array of data to each respective element in HTML?**
-    forecastTemp.textContent = "Temp: " + dTemp + '\u00B0F ';
-    forecastWind.textContent = "Wind: " + dailyWind + " MPH";
-    forecastHumid.textContent = "Humidity: " + dailyHumid + " %";
+    // // Consolidate data into array object
+    // arrayDataObject[i] = {
+    //   'time': dailyTime[i],
+    //   'icon': iconUrl[i],
+    //   'temp': dTemp[i],
+    //   'wind': dailyWind[i],
+    //   'humid': dailyHumid[i]
+    // }
+    // console.log('Date[i]:', arrayDataObject[i].time[i])
+    // console.log('Temp[i]:', arrayDataObject[i].dTemp[i])
+
+    // Create HTML elements for displaying data
+    var newCard = forecastCard.clone();
+    arrayCards[i + 1] = newCard;
+    newCard.append(forecastCard);
+    // lastCard=newCard;
+    // newCard.insertAfter(lastCard);
+    // Put data into HTML elements
+    arrayCards[i].forecastDate.textContent = arrayDataObject[i].time;
+    arrayCards[i].forecastIcon.setAttribute('src', arrayDataObject[i].icon);
+    arrayCards[i].forecastTemp.textContent = "Temp: " + arrayDataObject[i].temp + '\u00B0F ';
+    arrayCards[i].forecastWind.textContent = "Wind: " + arrayDataObject[i].wind + " MPH";
+    arrayCards[i].forecastHumid.textContent = "Humidity: " + arrayDataObject[i].humid + " %";
   }
 };
 
-// Function that reloads city input into search and pulls new weather data from the savedCity list
+// Handles user city input upon submit
+function handleFormsubmit(event) {
+  event.preventDefault();
+  var city = userCity.value.trim();
+  getUserInput(city);
+};
+
+// Reloads city input into search and pulls new weather data from the savedCity list
+function handleHistory(event) {
+  var historyButton = event.target;
+  var searchValue = historyButton.getAttribute("value");
+  geocode(searchValue);
+};
+
+form.addEventListener("submit", handleFormsubmit);
+
+searchResults.addEventListener("click", handleHistory);
